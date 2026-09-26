472b1cbbeb2d1f32105d42aa10028c4f384623bd
outakouboy-debug <outakouboy@gmail.com>
2026-09-26 10:56:33 +0000

    optimised the sort algo and corrected every printable text into perfect french

--- var.js
+++
@@ -1,8 +0,0 @@
-    for(let i=0;i<tab.length;i++){
-        for(let j=0;j<tab.length-1;j++){
-            if(tab[j].electeurs.length<tab[j+1].electeurs.length){  
-                [tab[j],tab[j+1]]=[tab[j+1],tab[j]];
-            }
-        }
-    }
-   
